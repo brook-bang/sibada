e9cbb37b271d2f914395ae80c36cc16336c7febf
brook-bang <1455047806@qq.com>
2025-02-10 17:47:19 +0800

    finish errors and rebuild group

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,36 @@
 use core::{
     fmt::Display,
     fmt::{self, Debug},
-  };
-  
-  #[derive(Debug, Default)]
-  pub enum ProofVerifyError {
+};
+use std::default;
+
+#[derive(Debug, Default)]
+pub enum ProofVerifyError {
     #[default]
     InternalError,
     DecompressionError([u8; 32]),
-  }
+}
+
+impl Display for ProofVerifyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            ProofVerifyError::DecompressionError(bytes) => write!(
+                f,
+                "Compressed group element failed to decompress: {bytes:?}",
+            ),
+            ProofVerifyError::InternalError => {
+                write!(f, "Proof verification failed",)
+            }
+        }
+    }
+}
+
+#[derive(Clone,Debug,Eq,PartialEq)]
+pub enum R1CSError {
+    NonPowerofTwoCons,
+    NonPowerofTwoVars,
+    InvalidNumberOfInputs,
+    InvalidNumberOfVars,
+    InvalidScalar,
+    InvalidIndex,
+}
--- src/group.rs
+++ src/group.rs
@@ -1,27 +1,59 @@
-use std::ops::MulAssign;
+use itertools::Group;
 
-use crate::{errors::ProofVerifyError, scalar::Scalar};
+use super::errors::ProofVerifyError;
+use super::scalar::{Scalar, ScalarBytes, ScalarBytesFromScalar};
+use core::borrow::Borrow;
+use core::ops::{Mul, MulAssign};
 
 pub type GroupElement = curve25519_dalek::ristretto::RistrettoPoint;
+
 pub type CompressedGroup = curve25519_dalek::ristretto::CompressedRistretto;
 
 pub trait CompressedGroupExt {
     type Group;
-    fn unpack(&self) -> Result<Self::Group,ProofVerifyError>;
+    fn unpack(&self) -> Result<Self::Group, ProofVerifyError>;
 }
 
 impl CompressedGroupExt for CompressedGroup {
     type Group = curve25519_dalek::ristretto::RistrettoPoint;
-    
-    fn unpack(&self) -> Result<Self::Group,ProofVerifyError> {
-        self.decompress().ok_or_else(|| ProofVerifyError::DecompressionError(self.to_bytes()))
-    }    
+
+    fn unpack(&self) -> Result<Self::Group, ProofVerifyError> {
+        self.decompress()
+            .ok_or_else(|| ProofVerifyError::DecompressionError(self.to_bytes()))
+    }
 }
 
-pub const GROUP_BASEPOINT_COMPRESSED: CompressedGroup = curve25519_dalek::constants::RISTRETTO_BASEPOINT_COMPRESSED;
+pub const GROUP_BASEPOINT_COMPRESSED: CompressedGroup =
+    curve25519_dalek::constants::RISTRETTO_BASEPOINT_COMPRESSED;
 
 impl<'b> MulAssign<&'b Scalar> for GroupElement  {
-    fn mul_assign(&mut self, rhs: &'b Scalar) {
-        todo!()
+    fn mul_assign(&mut self, scalar: &'b Scalar) {
+        let result = (self as &GroupElement) * Scalar::decompress_scalar(scalar);
+        *self = result;
     }
+}
+
+impl<'b> Mul<&'b Scalar> for &GroupElement {
+    type Output = GroupElement;
+
+    fn mul(self, scalar: &'b Scalar) -> GroupElement {
+        self * Scalar::decompress_scalar(scalar)
+    }
+}
+
+impl<'b> Mul<&'b GroupElement> for &Scalar {
+    type Output = GroupElement;
+
+    fn mul(self, point: &'b GroupElement) -> GroupElement {
+        Scalar::decompress_scalar(self) * point
+    }
+}
+
+pub trait VartimeMultiscalarMul {
+    type Scalar;
+    fn vartime_multiscalar_mul<I,J>(scalars: I, points: J) -> Self
+    where 
+    I:IntoIterator,
+    I::Item
+    
 }
